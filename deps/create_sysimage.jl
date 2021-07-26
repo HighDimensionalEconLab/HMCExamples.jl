3d281946294d4dc5f008aca57d602a373b649760
Jesse Perla <jesseperla@gmail.com>
2021-07-26 07:17:50 -0700

    Support for replacing default sysimage

--- deps/create_sysimage.jl
+++ deps/create_sysimage.jl
@@ -4,9 +4,11 @@ import Pkg, Libdl, PackageCompiler
 # crude CLI toggle. Either:
 # julia ./deps/create_sysimage.jl   # defaults
 # julia ./deps/create_sysimage.jl --only_package --no_precompile_tests   #change to only use the one package and not precompile tests, etc.
+# julia ./deps/create_sysimage.jl --replace_default # replace the default sysimage.  Ony intended for batch environments with vscode/etc.
 
 all_packages = !("--only_package" in ARGS)
 precompile_tests = !("--no_precompile_tests" in ARGS)
+replace_default = !("--replace_default" in ARGS)
 
 project = dirname(@__DIR__)  # Must be one level deep below the project file
 sysimage_path = joinpath(project, "JuliaSysimage.$(Libdl.dlext)")  # consistent naming with vscode
@@ -18,4 +20,10 @@ precompile_execution_file = precompile_tests ? joinpath(project, "test/runtests.
 precompile_tests && @info "Using unit tests for precompile execution file."
 @info "Precompiling packages: $packages: "
 @info "Now building a custom sysimage for the environment '$project'."
-PackageCompiler.create_sysimage(packages; sysimage_path, project, precompile_execution_file)
+if replace_default
+    @info "Now replacing the default sysimage with one for the environment '$project'"
+    PackageCompiler.create_sysimage(packages; replace_default, project, precompile_execution_file)
+else
+    @info "Now building a custom sysimage for the environment '$project' to $sysimage_path"
+    PackageCompiler.create_sysimage(packages; sysimage_path, project, precompile_execution_file)
+end
