21608026f52ac9782db964060d2040a1817f65c7
Jesse Perla <jesseperla@gmail.com>
2021-08-26 23:43:26 -0700

    Turned off most of the  FVGQ as well as the ones with figures

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,10 @@
 using HMCExamples, StatsPlots
+
 # The main purpose is for tracing the compiler, not for exection speed or results.  Keep the number of samples small
-HMCExamples.main_rbc_1_kalman(["--num_samples", "10"]) # only do one of them with the figures. Otherwise too slow.
-HMCExamples.main_rbc_1_joint(["--num_samples", "10", "--full_results", "false"])
-HMCExamples.main_rbc_2_joint(["--num_samples", "10", "--full_results", "false"])
-HMCExamples.main_FVGQ_1_kalman(["--num_samples", "10", "--full_results", "false"])
-HMCExamples.main_FVGQ_1_joint(["--num_samples", "10", "--full_results", "false"])
-HMCExamples.main_FVGQ_2_joint(["--num_samples", "10", "--full_results", "false"])
+#HMCExamples.main_rbc_1_kalman(["--num_samples", "10", "--max_depth", "2"]) # only do one of them with the figures. Otherwise too slow.
+HMCExamples.main_rbc_1_kalman(["--num_samples", "30", "--full_results", "false", "--max_depth", "2"]) # only do one of them with the figures. Otherwise too slow.
+HMCExamples.main_rbc_1_joint(["--num_samples", "30", "--full_results", "false", "--max_depth", "2"])
+HMCExamples.main_rbc_2_joint(["--num_samples", "30", "--full_results", "false", "--max_depth", "2"])
+HMCExamples.main_FVGQ_1_kalman(["--num_samples", "10", "--full_results", "false", "--max_depth", "2"])
+# HMCExamples.main_FVGQ_1_joint(["--num_samples", "10", "--full_results", "false", "--max_depth", "2"])
+# HMCExamples.main_FVGQ_2_joint(["--num_samples", "10", "--full_results", "false", "--max_depth", "2"])
