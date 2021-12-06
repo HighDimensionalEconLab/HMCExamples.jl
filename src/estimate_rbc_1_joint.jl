b2b1e37ccdf640c496000549c71faa69667936fa
Peifan Wu <wupeifan@gmail.com>
2021-12-05 23:46:41 -0800

    half way done
    wait for DSSM 0.4.1

--- src/estimate_rbc_1_joint.jl
+++ src/estimate_rbc_1_joint.jl
@@ -11,37 +11,30 @@ function estimate_rbc_1_joint(d)
     # use_tensorboard = true # could add toggle later
 
     # load data relative to the current path
-    data_path = joinpath(pkgdir(HMCExamples), d.data_path)
+    data_path = joinpath(pkgdir(HMCExamples), d[:data_path])
     df = Matrix(DataFrame(CSV.File(data_path)))
     z = [df[i, :] for i in 1:size(df, 1)]
     ϵ0 = Matrix(DataFrame(CSV.File(joinpath(pkgdir(HMCExamples), "data/epsilons_burnin_rbc_1.csv");header=false)))
     # Create the perturbation and the turing models
     m = PerturbationModel(HMCExamples.rbc)
-    p_d = (α = d.alpha, β = d.beta, ρ = d.rho)
-    p_f = (δ = d.delta, σ = d.sigma, Ω_1 = d.Omega_1)
-    c = SolverCache(model_rbc, Val(1), p_d)
-    turing_model = rbc_joint(
-        z, m, p_f, d.alpha_prior, d.beta_prior, d.rho_prior, c, PerturbationSolverSettings(;ϵ_BK = d.epsilon_BK, d.print_level, d.use_solution_cache)
-    )
+    p_d = (α = d[:alpha], β = d[:beta], ρ = d[:rho])
+    p_f = (δ = d[:delta], σ = d[:sigma], Ω_1 = d[:Omega_1])
+    c = SolverCache(m, Val(1), p_d)
+    turing_model = rbc_joint(z, m, p_f, d[:alpha_prior], d[:beta_prior], d[:rho_prior], c, PerturbationSolverSettings()
     # Sampler
     name = "rbc-joint-s$(d.num_samples)-seed$(d.seed)"
     include_vars = ["α", "β_draw", "ρ"]  # variables to log
     # callback = TensorBoardCallback(d.results_path; name, include=include_vars)
-    num_adapts = convert(Int64, floor(d.num_samples * d.adapts_burnin_prop))
+    num_adapts = convert(Int64, floor(d[:num_samples] * d[:adapts_burnin_prop]))
 
     Random.seed!(d.seed)
     @info "Generating $(d.num_samples) samples with $(num_adapts) adapts across $(d.num_chains) chains"
 
-    chain = sample(
-        turing_model,
-        NUTS(num_adapts, d.target_acceptance_rate; d.max_depth),
-        MCMCThreads(),
-        d.num_samples,
-        d.num_chains;
-        init_params=[p_d..., ϵ0],
-        progress=true,
-        save_state=true,
-        # callback,
+    chain = sample(turing_model, NUTS(num_adapts, d[:target_acceptance_rate]; max_depth = d[:max_depth]), d[:num_samples];
+          init_params=[p_d..., ϵ0],
+          progress=true,
+    #     save_state=true,
+    #     # callback,
     )
 
     # Store parameters in log directory
