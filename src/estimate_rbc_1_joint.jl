cf13e66b563bf2861a635ec081e8fccb3077a077
cpfiffer <cameron@pfiffer.org>
2021-12-09 10:25:40 -0800

    Multithreading code

--- src/estimate_rbc_1_joint.jl
+++ src/estimate_rbc_1_joint.jl
@@ -31,8 +31,12 @@ function estimate_rbc_1_joint(d)
     Random.seed!(d[:seed])
     @info "Generating $(d[:num_samples]) samples with $(num_adapts) adapts across $(d[:num_chains]) chains"
 
-    chain = sample(turing_model, NUTS(num_adapts, d[:target_acceptance_rate]; max_depth = d[:max_depth]),
-        d[:num_samples];
+    chain = sample(
+        turing_model,
+        NUTS(num_adapts, d[:target_acceptance_rate]; max_depth = d[:max_depth]),
+        MCMCThreads(),
+        d[:num_samples],
+        d[:num_chains];
         init_params=[p_d..., ϵ0],
         progress=true,
         save_state=true,
