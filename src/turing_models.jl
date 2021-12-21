1586c3e0b8b4aff7465f96b694b562878735d3f8
Peifan Wu <wupeifan@gmail.com>
2021-12-21 11:39:08 -0800

    #33

--- src/turing_models.jl
+++ src/turing_models.jl
@@ -36,7 +36,7 @@ end
     Turing.@addlogprob! solve(sol, sol.x_ergodic, (0, length(z)); observables = z).logpdf
 end
 
-@model function rbc_joint(z, m, p_f, α_prior, β_prior, ρ_prior, cache, settings, x0 = zeros(m.n_x))
+@model function rbc_joint(z, m, p_f, α_prior, β_prior, ρ_prior, cache::DifferentiableStateSpaceModels.AbstractSolverCache{Order}, settings, x0 = zeros(m.n_x)) where {Order}
     α ~ truncated(Normal(α_prior[1], α_prior[2]), α_prior[3], α_prior[4])
     β_draw ~ Gamma(β_prior[1], β_prior[2])
     ρ ~ Beta(ρ_prior[1], ρ_prior[2])
@@ -46,7 +46,7 @@ end
     T = length(z)
     ϵ_draw ~ MvNormal(m.n_ϵ * T, 1.0)
     ϵ = map(i -> ϵ_draw[((i - 1) * m.n_ϵ + 1):(i * m.n_ϵ)], 1:T)
-    sol = generate_perturbation(m, p_d, p_f, Val(1); cache)
+    sol = generate_perturbation(m, p_d, p_f, Val(Order); cache)
     (settings.print_level > 1) && println("Perturbation generated")
 
     if !(sol.retcode == :Success)
