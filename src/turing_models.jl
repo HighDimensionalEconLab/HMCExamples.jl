fcf2ceae1c1dfccdaec4d37e2c9fa8429213f62c
Peifan Wu <wupeifan@gmail.com>
2021-12-25 14:11:06 -0800

    all updated like a charm

--- src/turing_models.jl
+++ src/turing_models.jl
@@ -51,13 +51,13 @@ end
         )
 
         simulation = DifferenceEquations.solve(problem, KalmanFilter(); vectype = Zygote.Buffer)
-        @addlogprob! simulation.likelihood
+        @addlogprob! simulation.loglikelihood
     end
 
     return
 end
 
-@model function rbc_joint(z, m, p_f, α_prior, β_prior, ρ_prior, cache::DifferentiableStateSpaceModels.AbstractSolverCache{Order}, settings, x0 = zeros(m.n_x)) where {Order}
+@model function rbc_joint(z, m, p_f, α_prior, β_prior, ρ_prior, cache::DifferentiableStateSpaceModels.AbstractSolverCache{Order}, settings, x0) where {Order}
     α ~ truncated(Normal(α_prior[1], α_prior[2]), α_prior[3], α_prior[4])
     β_draw ~ Gamma(β_prior[1], β_prior[2])
     ρ ~ Beta(ρ_prior[1], ρ_prior[2])
@@ -90,7 +90,7 @@ end
         )
 
         simulation = DifferenceEquations.solve(problem, NoiseConditionalFilter(); vectype = Zygote.Buffer)
-        @addlogprob! simulation.likelihood
+        @addlogprob! simulation.loglikelihood
     end
     return
 end
@@ -146,67 +146,12 @@ end
         )
 
         simulation = DifferenceEquations.solve(problem, KalmanFilter(); vectype = Zygote.Buffer)
-		@addlogprob! simulation.likelihood
+		@addlogprob! simulation.loglikelihood
     end
     return
 end
 
-@model function FVGQ20_joint(z, m, p_f, params, cache, settings, x0 = zeros(m.n_x))
-    T = length(z)
-    # Priors
-    β_draw ~ Gamma(params.β[1], params.β[2])
-    β = 1 / (β_draw / 100 + 1)
-    h ~ Beta(params.h[1], params.h[2])
-    κ ~ truncated(Normal(params.κ[1], params.κ[2]), params.κ[3], params.κ[4])
-    χ ~ Beta(params.χ[1], params.χ[2])
-    γR ~ Beta(params.γR[1], params.γR[2])
-    γΠ ~ truncated(Normal(params.γΠ[1], params.γΠ[2]), params.γΠ[3], params.γΠ[4])
-    Πbar_draw ~ Gamma(params.Πbar[1], params.Πbar[2])
-    Πbar = Πbar_draw / 100 + 1
-    ρd ~ Beta(params.ρd[1], params.ρd[2])
-    ρφ ~ Beta(params.ρφ[1], params.ρφ[2])
-    ρg ~ Beta(params.ρg[1], params.ρg[2])
-    g_bar ~ Beta(params.g_bar[1], params.g_bar[2])
-    σ_A ~ InverseGamma(params.σ_A[1], params.σ_A[2])
-    σ_d ~ InverseGamma(params.σ_d[1], params.σ_d[2])
-    σ_φ ~ InverseGamma(params.σ_φ[1], params.σ_φ[2])
-    σ_μ ~ InverseGamma(params.σ_μ[1], params.σ_μ[2])
-    σ_m ~ InverseGamma(params.σ_m[1], params.σ_m[2])
-    σ_g ~ InverseGamma(params.σ_g[1], params.σ_g[2])
-    Λμ ~ Gamma(params.Λμ[1], params.Λμ[2])
-    ΛA ~ Gamma(params.ΛA[1], params.ΛA[2])
-    ϵ_draw ~ MvNormal(m.n_ϵ * T, 1.0)
-    ϵ = map(i -> ϵ_draw[((i-1)*m.n_ϵ+1):(i*m.n_ϵ)], 1:T)
-    # Likelihood
-    θ = (; β, h, κ, χ, γR, γΠ, Πbar, ρd, ρφ, ρg, g_bar, σ_A, σ_d, σ_φ, σ_μ, σ_m, σ_g, Λμ, ΛA)
-    (settings.print_level > 0) && @show θ
-    sol = generate_perturbation(m, θ, p_f, Val(1); cache)
-    if !(sol.retcode == :Success)
-        @addlogprob! -Inf
-    else
-        z_trend = params.Hx * sol.x + params.Hy * sol.y
-        z_detrended = map(i -> z[i] - z_trend, eachindex(z))
-
-        # Simulate and get the likelihood.
-        problem = StateSpaceProblem(
-            DifferentiableStateSpaceModels.dssm_evolution,
-            DifferentiableStateSpaceModels.dssm_volatility,
-            DifferentiableStateSpaceModels.dssm_observation,
-            x0,
-            (0, T),
-            sol,
-            noise = DefinedNoise(ϵ),
-            obs_noise = sol.D,
-            observables = z_detrended
-        )
-
-        simulation = DifferenceEquations.solve(problem, NoiseConditionalFilter(); vectype = Zygote.Buffer)
-        @addlogprob! simulation.likelihood
-      end
-      return
-end
-
-@model function FVGQ20_joint(z, m, p_f, params, cache::DifferentiableStateSpaceModels.AbstractSolverCache{Order}, settings, x0 = zeros(m.n_x)) where {Order}
+@model function FVGQ20_joint(z, m, p_f, params, cache::DifferentiableStateSpaceModels.AbstractSolverCache{Order}, settings, x0) where {Order}
     T = length(z)
     # Priors
     β_draw ~ Gamma(params.β[1], params.β[2])
@@ -258,7 +203,7 @@ end
         )
 
         simulation = DifferenceEquations.solve(problem, NoiseConditionalFilter(); vectype=Zygote.Buffer)
-        @addlogprob! simulation.likelihood
+        @addlogprob! simulation.loglikelihood
     end
     return
 end
