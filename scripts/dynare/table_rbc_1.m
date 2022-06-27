7a0e718d467a1c0156ae25e1e63ba4c99b5c2b23
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2022-06-27 13:07:56 -0700

    adjust dynare scriptname

--- scripts/dynare/table_rbc_1.m
+++ scripts/dynare/table_rbc_1.m
@@ -1,6 +1,6 @@
 addpath("dynare-5.1/matlab");
 tic
-dynare rbc_1.mod;
+dynare rbc_1_table.mod;
 rt = toc;
 load("rbc_1/metropolis/rbc_1_mh1_blck1.mat");
 save("dynare_chains_timed/chain_1st_order.mat", "logpo2", "x2", "rt");
