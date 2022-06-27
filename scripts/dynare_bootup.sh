0237deedfb06187e1a76885fa3f8e6c534ec45a3
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2022-06-27 12:26:58 -0700

    Delete dynare_bootup.sh

--- scripts/dynare_bootup.sh
+++
@@ -1,7 +0,0 @@
-wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/containerd.io_1.6.6-1_amd64.deb
-wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce_20.10.9~3-0~ubuntu-bionic_amd64.deb
-wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce-cli_20.10.9~3-0~ubuntu-bionic_amd64.deb
-
-sudo dpkg -i containerd.io_1.6.6-1_amd64.deb
-sudo dpkg -i docker-ce-cli_20.10.9~3-0~ubuntu-bionic_amd64.deb
-sudo dpkg -i docker-ce_20.10.9~3-0~ubuntu-bionic_amd64.deb
