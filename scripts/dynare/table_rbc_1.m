757129adb95777a6b5d9d30bde87396c867d93a9
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2022-06-27 14:43:15 -0700

    fix filepath

--- scripts/dynare/table_rbc_1.m
+++ scripts/dynare/table_rbc_1.m
@@ -2,5 +2,5 @@ addpath("dynare-5.1/matlab");
 tic
 dynare rbc_1_table.mod;
 rt = toc;
-load("rbc_1/metropolis/rbc_1_mh1_blck1.mat");
+load("rbc_1_table/metropolis/rbc_1_mh1_blck1.mat");
 save("dynare_chains_timed/chain_1st_order.mat", "logpo2", "x2", "rt");
