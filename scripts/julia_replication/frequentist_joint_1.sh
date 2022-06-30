fda70c5a7cf157ab2aac92596e54d676ae9f8377
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2022-06-30 13:35:19 -0700

    Julia replication files (#90)
    * Create f124_plots.sh
    
    * Create rbc_1_kalman_burnin_dynare.csv
    
    * Create install_julia.sh
    
    * remove printouts
    
    * Create frequentist.sh
    
    * Create generate_fake_data_ergodic_frequentist.jl
    
    * Create rbc_1_kalman_burnin_ergodic_100.csv
    
    * Create rbc_1_joint_burnin_ergodic_50.csv
    
    * Delete rbc_1_kalman_burnin_ergodic_100.csv
    
    * Create rbc_1_joint_burnin_ergodic_100.csv
    
    * Create rbc_2_joint_burnin_ergodic_100.csv
    
    * Create rbc_2_joint_burnin_ergodic_50.csv
    
    * adjust filepaths
    
    * Update and rename frequentist.sh to frequentist_kalman.sh
    
    * Create frequentist_joint_1.sh
    
    * Create frequentist_joint_2.sh
    
    * adjust shutdown command
    
    * adjust shutdown command
    
    * Create robustness.sh
    
    * adjust shutdown command
    
    * adjust shutdown command
    
    * Create tables.sh
    
    * fix typo
    
    * fix typo
    
    * fix typo
    
    * fix typo
    
    * fix datapaths
    
    * adjust julia instantiate path
    
    * fix more typos
    
    * fix more typos
    
    * fix more typos
    
    * remove leftover comment
    
    * remove leftover comment
    
    * remove leftover comment

---
+++ scripts/julia_replication/frequentist_joint_1.sh
@@ -0,0 +1,31 @@
+# instantiate project
+echo "***** Instantiating Project *****"
+~/julia-1.7.1/bin/julia --project -e "using Pkg; Pkg.instantiate()"
+
+# execute estimation scripts
+for i in `seq 10`
+    do for j in `seq 10`
+        do echo "EXECUTING fit_rbc_1_joint.jl WITH seed $((10 * (i - 1) + j)) T = 50"
+        ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_1_joint.jl --results_path ./.results/frequentist_rbc_1_joint_$((10 * (i - 1) + j)) --overwrite_results true --num_samples 5000 --seed $((10 * (i - 1) + j)) --data_path .data/rbc_1_$((10 * (i - 1) + j))_50.csv --init_params_file data/rbc_1_joint_burnin_ergodic_50.csv &
+    done
+    wait
+done
+
+for i in `seq 10`
+    do for j in `seq 10`
+        do echo "EXECUTING fit_rbc_1_joint.jl WITH seed $((10 * (i - 1) + j)) T = 100"
+        ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_1_joint.jl --results_path ./.results/frequentist_rbc_1_joint_$((10 * (i - 1) + j)) --overwrite_results true --num_samples 5000 --seed $((10 * (i - 1) + j)) --data_path .data/rbc_1_$((10 * (i - 1) + j))_100.csv --init_params_file data/rbc_1_joint_burnin_ergodic_100.csv &
+    done
+    wait
+done
+
+for i in `seq 10`
+    do for j in `seq 10`
+        do echo "EXECUTING fit_rbc_1_joint.jl WITH seed $((10 * (i - 1) + j)) T = 200"
+        ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_1_joint.jl --results_path ./.results/frequentist_rbc_1_joint_$((10 * (i - 1) + j)) --overwrite_results true --num_samples 5000 --seed $((10 * (i - 1) + j)) --data_path .data/rbc_1_$((10 * (i - 1) + j))_200.csv --init_params_file data/rbc_1_joint_burnin_ergodic.csv &
+    done
+    wait
+done
+
+echo "FINISHED"
+sudo shutdown -h now
