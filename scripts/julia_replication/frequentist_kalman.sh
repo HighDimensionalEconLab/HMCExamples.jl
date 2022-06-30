6829d95743c24f0b9a874956dbb76a3864f01169
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2022-06-30 13:16:01 -0700

    fix typo

--- scripts/julia_replication/frequentist_kalman.sh
+++ scripts/julia_replication/frequentist_kalman.sh
@@ -3,7 +3,7 @@ echo "***** Instantiating Project *****"
 julia --project -e "using Pkg; Pkg.instantiate()"
 
 # execute estimation scripts
-do for i in `seq 10`
+for i in `seq 10`
     do for j in `seq 10`
         do echo "EXECUTING fit_rbc_1_kalman.jl WITH seed $((10 * (i - 1) + j)) T = 50"
         ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_1_kalman.jl --results_path ./.results/frequentist_rbc_1_kalman_$((10 * (i - 1) + j)) --overwrite_results true --num_samples 5000 --seed $((10 * (i - 1) + j)) --data_path .data/rbc_1_$((10 * (i - 1) + j))_50.csv --init_params_file data/rbc_1_kalman_burnin_dynare &
@@ -11,7 +11,7 @@ do for i in `seq 10`
     wait
 done
 
-do for i in `seq 10`
+for i in `seq 10`
     do for j in `seq 10`
         do echo "EXECUTING fit_rbc_1_kalman.jl WITH seed $((10 * (i - 1) + j)) T = 100"
         # generate observables here
@@ -20,7 +20,7 @@ do for i in `seq 10`
     wait
 done
 
-do for i in `seq 10`
+for i in `seq 10`
     do for j in `seq 10`
         do echo "EXECUTING fit_rbc_1_kalman.jl WITH seed $((10 * (i - 1) + j)) T = 200"
         # generate observables here
