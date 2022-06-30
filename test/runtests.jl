89d656e01cfb6a8842136e481ff6a8d37192b9e9
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2022-06-30 14:45:28 -0700

    add init params files to scripts

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,14 @@
 using HMCExamples
 
 # The main purpose is for tracing the compiler, not for execution speed or results.  Keep the number of samples small
-HMCExamples.main_rbc_1_kalman(["--num_samples", "30", "--max_depth", "2", "--results_path", "./.results/rbc_1_kalman_test", "--overwrite_results", "true"])
+HMCExamples.main_rbc_1_kalman(["--num_samples", "30", "--max_depth", "2", "--results_path", "./.results/rbc_1_kalman_test", "--overwrite_results", "true", "--init_params_file", "data/rbc_1_kalman_burnin_dynare.csv"])
 #HMCExamples.main_rbc_1_kalman(["--num_samples", "100", "--max_depth", "2", "--print_level", "2", "--results_path", "./.results/rbc_1_kalman_test_2", "--overwrite_results", "true"])
 #HMCExamples.main_rbc_1_kalman(["--num_samples", "60", "--max_depth", "2", "--results_path", "./.results/rbc_1_kalman_test_chains", "--overwrite_results", "true", "--num_chains", "4"])
-HMCExamples.main_rbc_1_joint(["--num_samples", "30", "--max_depth", "2", "--results_path", "./.results/rbc_1_joint_test", "--overwrite_results", "true", "--init_params_file", ""])
+HMCExamples.main_rbc_1_joint(["--num_samples", "30", "--max_depth", "2", "--results_path", "./.results/rbc_1_joint_test", "--overwrite_results", "true", "--init_params_file", "data/rbc_1_joint_burnin_ergodic.csv"])
 #HMCExamples.main_rbc_1_joint(["--num_samples", "30", "--max_depth", "2", "--results_path", "./.results/rbc_1_joint_test_discard", "--overwrite_results", "true", "--discard_initial", "10","--init_params_file", ""])
 #HMCExamples.main_rbc_1_joint(["--num_samples", "30", "--max_depth", "2", "--results_path", "./.results/rbc_1_joint_test_use_burnin", "--overwrite_results", "true", "--init_params_file", "data/rbc_1_joint_burnin.csv"])
 
-HMCExamples.main_rbc_2_joint(["--num_samples", "30", "--max_depth", "2", "--results_path", "./.results/rbc_2_joint_test", "--overwrite_results", "true"])
+HMCExamples.main_rbc_2_joint(["--num_samples", "30", "--max_depth", "2", "--results_path", "./.results/rbc_2_joint_test", "--overwrite_results", "true", "--init_params_file", "data/rbc_2_joint_burnin_ergodic.csv"])
 
 # #Need to call with the -O1 compilation is working with PackageCompiler at this point
 HMCExamples.parse_commandline_FVGQ_1_kalman(ARGS)
