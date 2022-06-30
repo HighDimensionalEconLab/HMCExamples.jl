4040f961e971fc3cf90590963f15db400613e9ab
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2022-06-30 12:49:43 -0700

    adjust filepaths

--- scripts/julia_replication/generate_fake_data_ergodic_frequentist.jl
+++ scripts/julia_replication/generate_fake_data_ergodic_frequentist.jl
@@ -16,7 +16,7 @@ for T in [50, 100, 200]
         prob = LinearStateSpaceProblem(mod_perturb, x0, (0, T))
         sol = solve(prob, DirectIteration())
         z_vec = VectorOfArray(sol.z)
-        CSV.write(joinpath(pkgdir(HMCExamples), string(".data/rbc_1_", counter, "_", T, "_.csv")), DataFrame(c_obs=z_vec[1, :], i_obs=z_vec[2, :]))
+        CSV.write(joinpath(pkgdir(HMCExamples), string(".data/rbc_1_", counter, "_", T, ".csv")), DataFrame(c_obs=z_vec[1, :], i_obs=z_vec[2, :]))
 
         mod_perturb_2 = generate_perturbation(m_2, p_d, p_f, Val(2))
         println("second-order x_ergodic_var:", mod_perturb_2.x_ergodic_var)
@@ -24,6 +24,6 @@ for T in [50, 100, 200]
         prob_2 = QuadraticStateSpaceProblem(mod_perturb_2, x0_2, (0, T))
         sol_2 = solve(prob_2, DirectIteration())
         z_vec_2 = VectorOfArray(sol_2.z)
-        CSV.write(joinpath(pkgdir(HMCExamples), string(".data/rbc_2_", counter, "_", T, "_.csv")), DataFrame(c_obs=z_vec_2[1, :], i_obs=z_vec_2[2, :]))
+        CSV.write(joinpath(pkgdir(HMCExamples), string(".data/rbc_2_", counter, "_", T, ".csv")), DataFrame(c_obs=z_vec_2[1, :], i_obs=z_vec_2[2, :]))
     end
 end
