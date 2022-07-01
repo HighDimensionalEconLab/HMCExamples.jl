16319655c12362e73d902a83d37565725309f095
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2022-07-01 14:52:22 -0700

    separate results paths

--- scripts/julia_replication/frequentist_kalman.sh
+++ scripts/julia_replication/frequentist_kalman.sh
@@ -7,7 +7,7 @@ mkdir .data
 for i in `seq 10`
     do for j in `seq 10`
         do echo "EXECUTING fit_rbc_1_kalman.jl WITH seed $((10 * (i - 1) + j)) T = 50"
-        ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_1_kalman.jl --results_path ./.results/frequentist_rbc_1_kalman_$((10 * (i - 1) + j)) --overwrite_results true --num_samples 5000 --seed $((10 * (i - 1) + j)) --data_path .data/rbc_1_$((10 * (i - 1) + j))_50.csv --init_params_file data/rbc_1_kalman_burnin_dynare.csv &
+        ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_1_kalman.jl --results_path ./.results/frequentist_rbc_1_kalman_$((10 * (i - 1) + j))_50 --overwrite_results true --num_samples 5000 --seed $((10 * (i - 1) + j)) --data_path .data/rbc_1_$((10 * (i - 1) + j))_50.csv --init_params_file data/rbc_1_kalman_burnin_dynare.csv &
     done
     wait
 done
@@ -15,7 +15,7 @@ done
 for i in `seq 10`
     do for j in `seq 10`
         do echo "EXECUTING fit_rbc_1_kalman.jl WITH seed $((10 * (i - 1) + j)) T = 100"
-        ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_1_kalman.jl --results_path ./.results/frequentist_rbc_1_kalman_$((10 * (i - 1) + j)) --overwrite_results true --num_samples 5000 --seed $((10 * (i - 1) + j)) --data_path .data/rbc_1_$((10 * (i - 1) + j))_100.csv --init_params_file data/rbc_1_kalman_burnin_dynare.csv &
+        ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_1_kalman.jl --results_path ./.results/frequentist_rbc_1_kalman_$((10 * (i - 1) + j))_100 --overwrite_results true --num_samples 5000 --seed $((10 * (i - 1) + j)) --data_path .data/rbc_1_$((10 * (i - 1) + j))_100.csv --init_params_file data/rbc_1_kalman_burnin_dynare.csv &
     done
     wait
 done
@@ -23,7 +23,7 @@ done
 for i in `seq 10`
     do for j in `seq 10`
         do echo "EXECUTING fit_rbc_1_kalman.jl WITH seed $((10 * (i - 1) + j)) T = 200"
-        ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_1_kalman.jl --results_path ./.results/frequentist_rbc_1_kalman_$((10 * (i - 1) + j)) --overwrite_results true --num_samples 5000 --seed $((10 * (i - 1) + j)) --data_path .data/rbc_1_$((10 * (i - 1) + j))_200.csv --init_params_file data/rbc_1_kalman_burnin_dynare.csv &
+        ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_1_kalman.jl --results_path ./.results/frequentist_rbc_1_kalman_$((10 * (i - 1) + j))_200 --overwrite_results true --num_samples 5000 --seed $((10 * (i - 1) + j)) --data_path .data/rbc_1_$((10 * (i - 1) + j))_200.csv --init_params_file data/rbc_1_kalman_burnin_dynare.csv &
     done
     wait
 done
