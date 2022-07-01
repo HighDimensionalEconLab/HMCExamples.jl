5cf417064e2273156ce76ed372cb9a2b84435065
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2022-07-01 14:52:04 -0700

    separate results paths

--- scripts/julia_replication/frequentist_joint_2.sh
+++ scripts/julia_replication/frequentist_joint_2.sh
@@ -7,7 +7,7 @@ mkdir .data
 for i in `seq 5`
     do for j in `seq 10`
         do echo "EXECUTING fit_rbc_2_joint.jl WITH seed $((10 * (i - 1) + j)) T = 50"
-        ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_2_joint.jl --results_path ./.results/frequentist_rbc_2_joint_$((10 * (i - 1) + j)) --overwrite_results true --num_samples 5000 --seed $((10 * (i - 1) + j)) --data_path .data/rbc_2_$((10 * (i - 1) + j))_50.csv --init_params_file data/rbc_2_joint_burnin_ergodic_50.csv &
+        ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_2_joint.jl --results_path ./.results/frequentist_rbc_2_joint_$((10 * (i - 1) + j))_50 --overwrite_results true --num_samples 5000 --seed $((10 * (i - 1) + j)) --data_path .data/rbc_2_$((10 * (i - 1) + j))_50.csv --init_params_file data/rbc_2_joint_burnin_ergodic_50.csv &
     done
     wait
 done
@@ -15,7 +15,7 @@ done
 for i in `seq 5`
     do for j in `seq 10`
         do echo "EXECUTING fit_rbc_2_joint.jl WITH seed $((10 * (i - 1) + j)) T = 100"
-        ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_2_joint.jl --results_path ./.results/frequentist_rbc_2_joint_$((10 * (i - 1) + j)) --overwrite_results true --num_samples 5000 --seed $((10 * (i - 1) + j)) --data_path .data/rbc_2_$((10 * (i - 1) + j))_100.csv --init_params_file data/rbc_2_joint_burnin_ergodic_100.csv &
+        ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_2_joint.jl --results_path ./.results/frequentist_rbc_2_joint_$((10 * (i - 1) + j))_100 --overwrite_results true --num_samples 5000 --seed $((10 * (i - 1) + j)) --data_path .data/rbc_2_$((10 * (i - 1) + j))_100.csv --init_params_file data/rbc_2_joint_burnin_ergodic_100.csv &
     done
     wait
 done
@@ -23,7 +23,7 @@ done
 for i in `seq 5`
     do for j in `seq 10`
         do echo "EXECUTING fit_rbc_2_joint.jl WITH seed $((10 * (i - 1) + j)) T = 200"
-        ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_2_joint.jl --results_path ./.results/frequentist_rbc_2_joint_$((10 * (i - 1) + j)) --overwrite_results true --num_samples 5000 --seed $((10 * (i - 1) + j)) --data_path .data/rbc_2_$((10 * (i - 1) + j))_200.csv --init_params_file data/rbc_2_joint_burnin_ergodic.csv &
+        ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_2_joint.jl --results_path ./.results/frequentist_rbc_2_joint_$((10 * (i - 1) + j))_200 --overwrite_results true --num_samples 5000 --seed $((10 * (i - 1) + j)) --data_path .data/rbc_2_$((10 * (i - 1) + j))_200.csv --init_params_file data/rbc_2_joint_burnin_ergodic.csv &
     done
     wait
 done
