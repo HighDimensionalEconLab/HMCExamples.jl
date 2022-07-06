423241152d5f0932e7f1e01a7a04c14b620305ec
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2022-07-05 22:13:38 -0700

    fix filepaths

--- scripts/figplots/parse_dynare_log.py
+++ scripts/figplots/parse_dynare_log.py
@@ -5,8 +5,8 @@ n_rho = 5
 alpha_list = np.linspace(0.25, 0.45, n_alpha)
 beta_list = np.linspace(0.1, 0.4, n_beta)
 rho_list = np.linspace(0.3, 0.95, n_rho)
-with open("benchmarks_dynare/dynare_log") as f:
-    with open("benchmarks_dynare/parsed_log", "w") as f2:
+with open(".experiments/benchmarks_dynare/dynare_log") as f:
+    with open(".results/parsed_log", "w") as f2:
         for line in f:
             if len(line) >= 5 and line[1] == "_" and line[3] == "_":
                 f2.write(f"alpha = {round(alpha_list[int(line[0]) - 1], 4)}, beta_draw = {round(beta_list[int(line[2]) - 1], 4)}, rho = {round(rho_list[int(line[4]) - 1], 4)}\n")
