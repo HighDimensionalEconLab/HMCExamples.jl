13892b7596e22de79341da76a1dd84b72b717a8f
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2022-07-07 11:22:59 -0700

    add force-install flag to apt call

--- scripts/dynare_replication/dynare_bootup.sh
+++ scripts/dynare_replication/dynare_bootup.sh
@@ -3,7 +3,7 @@ wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/doc
 wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce-cli_20.10.9~3-0~ubuntu-bionic_amd64.deb
 
 sudo apt update
-sudo apt install libseccomp2:amd64 --upgrade
+sudo apt install -y libseccomp2:amd64 --upgrade
 
 sudo dpkg -i containerd.io_1.6.6-1_amd64.deb
 sudo dpkg -i docker-ce-cli_20.10.9~3-0~ubuntu-bionic_amd64.deb
