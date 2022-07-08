7dee420a392e13b1c26ad84a42cf8ffe5ff9b0fe
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2022-07-08 13:04:06 -0700

    pass altered perturb_covariance for joints on CLI

--- scripts/julia_replication/robustness.sh
+++ scripts/julia_replication/robustness.sh
@@ -7,9 +7,9 @@ for alpha in 0.25 0.3 0.35 0.4 0.45
             do echo "EXECUTING KALMAN WITH alpha $alpha beta_draw $beta_draw rho $rho"
             ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_1_kalman.jl --results_path ./.results/robustness_rbc_1_kalman_${alpha//./_}${beta_draw//./_}${rho//./_} --overwrite_results true --num_samples 6500 --seed 1 --init_params_file .data/rbc_1_kalman_burnin_dynare_${alpha//./_}${beta_draw//./_}${rho//./_}.csv
             echo "1 JOINT"
-            ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_1_joint.jl --results_path ./.results/robustness_rbc_1_joint_${alpha//./_}${beta_draw//./_}${rho//./_} --overwrite_results true --num_samples 4000 --seed 1 --init_params_file .data/rbc_1_joint_burnin_ergodic_${alpha//./_}${beta_draw//./_}${rho//./_}.csv
+            ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_1_joint.jl --results_path ./.results/robustness_rbc_1_joint_${alpha//./_}${beta_draw//./_}${rho//./_} --overwrite_results true --num_samples 4000 --seed 1 --perturb_covariance 1e-12 --init_params_file .data/rbc_1_joint_burnin_ergodic_${alpha//./_}${beta_draw//./_}${rho//./_}.csv
             echo "2 JOINT"
-            ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_2_joint.jl --results_path ./.results/robustness_rbc_2_joint_${alpha//./_}${beta_draw//./_}${rho//./_} --overwrite_results true --num_samples 4000 --seed 1 --init_params_file .data/rbc_2_joint_burnin_ergodic_${alpha//./_}${beta_draw//./_}${rho//./_}.csv
+            ~/julia-1.7.1/bin/julia --project --sysimage JuliaSysimage.so --threads auto -O1 bin/fit_rbc_2_joint.jl --results_path ./.results/robustness_rbc_2_joint_${alpha//./_}${beta_draw//./_}${rho//./_} --overwrite_results true --num_samples 4000 --seed 1 --perturb_covariance 1e-12 --init_params_file .data/rbc_2_joint_burnin_ergodic_${alpha//./_}${beta_draw//./_}${rho//./_}.csv
             echo "DONE"
         done
     done
