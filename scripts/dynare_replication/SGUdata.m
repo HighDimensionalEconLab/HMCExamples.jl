00633824310f8d3e67ec84ee9e62757631729b13
David Childers <38698381+donskerclass@users.noreply.github.com>
2022-08-04 18:08:33 -0400

    SGU dynare files
    Mod file for SGU with modifications for estimation, and a file to create the simulated data. Currently beta is broken by trying to change to beta_draw parameterization.

---
+++ scripts/dynare_replication/SGUdata.m
@@ -0,0 +1,25 @@
+%SGUdata.m
+% Takes output of dynare simulations from SU03ext.mod and stores a data set
+
+% Create mode file
+
+parameter_names = [{'alpha'};{'gamma'};{'psi'};{'beta_draw'};{'rho'};{'rho_u'};{'rho_v'}];
+xparam1 = [0.4;1.0;0.000742;0.25;0.4;0.4;0.4];
+
+save('SU03ext_mode.mat','parameter_names','xparam1')
+
+rng(54321); %Set seed
+dynare SU03ext.mod
+
+
+data1 = table(y_obs,ca_obs,r_obs);
+
+writetable(data1,'SGUsimorder1.csv')
+
+rng(54321); %Set seed
+dynare SU03ext.mod order=2
+
+data2 = table(y_obs,ca_obs,r_obs); 
+
+writetable(data2,'SGUsimorder2.csv')
+
