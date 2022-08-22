f19770a8e9535d307d15e53c6bcd40429e9a3910
Jesse Perla <jesseperla@gmail.com>
2022-08-22 15:39:07 -0700

    Fix for logging

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,6 +18,10 @@ HMCExamples.parse_commandline_FVGQ_2_joint(ARGS)
 # HMCExamples.main_FVGQ_1_joint(["--num_samples", "20", "--max_depth", "2", "--print_level", "1", "--results_path", "./results/main_FVGQ_1_joint", "--overwrite_results", "true"])
 # HMCExamples.main_FVGQ_2_joint(["--num_samples", "20", "--max_depth", "2", "--results_path", "./results/main_FVGQ_2_joint", "--overwrite_results", "true"])
 
-HMCExamples.main_sgu_1_kalman(["--num_samples", "30", "--max_depth", "2", "--results_path", "./.results/sgu_1_kalman_test", "--overwrite_results", "true", "--init_params_file", "data/sgu_1_kalman_burnin.csv"])
-HMCExamples.main_sgu_1_joint(["--num_samples", "30", "--max_depth", "2", "--results_path", "./.results/sgu_1_joint_test", "--overwrite_results", "true", "--init_params_file", "data/sgu_1_joint_burnin.csv"])
-HMCExamples.main_sgu_2_joint(["--num_samples", "30", "--max_depth", "2", "--results_path", "./.results/sgu_2_joint_test", "--overwrite_results", "true", "--init_params_file", "data/sgu_2_joint_burnin.csv"])
+HMCExamples.parse_commandline_sgu_1_kalman(ARGS)
+HMCExamples.parse_commandline_sgu_1_joint(ARGS)
+HMCExamples.parse_commandline_sgu_2_joint(ARGS)
+
+# HMCExamples.main_sgu_1_kalman(["--num_samples", "30", "--max_depth", "2", "--results_path", "./.results/sgu_1_kalman_test", "--overwrite_results", "true", "--init_params_file", "data/sgu_1_kalman_burnin.csv"])
+# HMCExamples.main_sgu_1_joint(["--num_samples", "30", "--max_depth", "2", "--results_path", "./.results/sgu_1_joint_test", "--overwrite_results", "true", "--init_params_file", "data/sgu_1_joint_burnin.csv"])
+# HMCExamples.main_sgu_2_joint(["--num_samples", "30", "--max_depth", "2", "--results_path", "./.results/sgu_2_joint_test", "--overwrite_results", "true", "--init_params_file", "data/sgu_2_joint_burnin.csv"])
