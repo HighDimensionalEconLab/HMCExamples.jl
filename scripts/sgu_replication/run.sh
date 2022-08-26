ad6e68838fcd4781ccbc0e021fcd0f4f97215996
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2022-08-26 11:51:13 -0700

    reorder sgu replication script

--- scripts/sgu_replication/run.sh
+++ scripts/sgu_replication/run.sh
@@ -1,9 +1,9 @@
 echo "**PRECOMPILING**"
+julia --project -e "using Pkg; Pkg.instantiate()"
 julia --threads auto -e 'using Pkg; Pkg.add("PackageCompiler")'
 julia --project --threads auto -O1 ./deps/create_sysimage.jl
 
 echo "**RUNNING SCRIPT**"
-julia --project -e "using Pkg; Pkg.instantiate()"
 
 julia --project --sysimage JuliaSysimage.dll --threads auto -O1 bin/fit_sgu_1_kalman.jl --results_path ./.results/sgu_1_kalman_small_noise --overwrite_results true --data_path data/sgu_1_draw_low.csv --Omega_1 0.00316 --num_samples 5000 --seed 0 --init_params_file data/sgu_1_kalman_burnin.csv
 
