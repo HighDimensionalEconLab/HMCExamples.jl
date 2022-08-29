0a6959659615409af055ab05c570c3d5ff7a97dd
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2022-08-28 20:00:25 -0700

    current state of plotting file

--- scripts/sgu_replication/statplots.jl
+++ scripts/sgu_replication/statplots.jl
@@ -1,6 +1,6 @@
 using HDF5, MCMCChains, MCMCChainsStorage, CSV, DataFrames, StatsPlots, Measures, Dates
 
-function generate_plots(batch, include_vars)
+function generate_plots(batch, shocks, include_vars)
     println("generating plots for ", batch)
     chain = h5open(".experiments/sgu/sgu_$(batch)/chain.h5", "r") do f
         read(f, Chains)
@@ -15,7 +15,7 @@ function generate_plots(batch, include_vars)
     density_plot = density(chain[include_vars], left_margin = 20mm, top_margin = 5mm, bottom_margin = 10mm)
     savefig(density_plot, ".figures/densityplots_sgu_$(batch).png")
 
-    if batch != "1_kalman"
+    if batch != "1_kalman_high_noise"
         # epsilons (Figure 6)
         symbol_to_int(s) = parse(Int, replace(string(s), "ϵ_draw["=>"", "]"=>""))
         symbollist = reshape([Symbol("ϵ_draw[$a]") for a in 1:600], 3, 200)
@@ -23,7 +23,7 @@ function generate_plots(batch, include_vars)
         plots = []
 
         # Import the true shock values
-        ϵ_true = Matrix(DataFrame(CSV.File("data/rbc_$(batch)_shocks.csv")))
+        ϵ_true = Matrix(DataFrame(CSV.File(shocks)))
 
         for i in 1:3
             ϵ_chain = sort(chain[:, symbollist[i, :], 1], lt = (x,y) -> symbol_to_int(x) < symbol_to_int(y))
@@ -33,8 +33,7 @@ function generate_plots(batch, include_vars)
             
             # Plot and save
             ϵ_plot = plot(ϵ_mean[2:end], ribbon=2 * ϵ_std[2:end], title = labels[i])
-            # may need to double-check the dimensions here
-            ϵ_plot = plot!(ϵ_true[i, :], label="True values")
+            ϵ_plot = plot!(ϵ_true[:, i], label="True values")
             push!(plots, ϵ_plot)
         end
         ϵ_plot = plot(plots[1], plots[2], plots[3], layout = (6, 1), size = (1600, 900), left_margin = 10mm, bottom_margin = 5mm, legend = false)
@@ -44,6 +43,6 @@ function generate_plots(batch, include_vars)
     println("  plots complete")
 end
 
-generate_plots("1_kalman", ["α", "γ", "ψ", "β_draw", "ρ", "ρ_u", "ρ_v"])
-generate_plots("1_joint", ["α", "γ", "ψ", "β_draw", "ρ", "ρ_u", "ρ_v"])
-generate_plots("2_joint", ["α", "γ", "ψ", "β_draw", "ρ", "ρ_u", "ρ_v"])
+generate_plots("1_kalman", "", ["α", "γ", "ψ", "β_draw", "ρ", "ρ_u", "ρ_v"])
+generate_plots("1_joint", "data/sgu_1_fixed_high_shocks.csv", ["α", "γ", "ψ", "β_draw", "ρ", "ρ_u", "ρ_v"])
+generate_plots("2_joint", "data/sgu_2_fixed_high_shocks.csv", ["α", "γ", "ψ", "β_draw", "ρ", "ρ_u", "ρ_v"])
