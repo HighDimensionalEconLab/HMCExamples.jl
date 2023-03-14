5a5b8f41f2b43707d6afa44f5d7d7911fda59ae6
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2023-03-14 13:54:27 -0700

    RBC SV server replication files

---
+++ scripts/rbc_sv_replication/install_julia.sh
@@ -0,0 +1,6 @@
+wget https://julialang-s3.julialang.org/bin/linux/x64/1.7/julia-1.7.1-linux-x86_64.tar.gz
+tar zxvf julia-1.7.1-linux-x86_64.tar.gz
+echo 'export PATH="$PATH:~/julia-1.7.1/bin"' > ~/.bashrc
+echo 'export PATH="$PATH:~/julia-1.7.1/bin"' > ~/.bash_profile
+source ~/.bashrc
+source ~/.bash_profile
